325441eec983f225afe1f78d598bac3cd789202a
Rohit0cs <sakharkar004@gmail.com>
2023-08-17 15:49:44 +0530

    java program

---
+++ java/time.java
@@ -0,0 +1,12 @@
+public class time {
+    public static void main(String[] args) {
+        int hour =2;
+        int minutes=5;
+
+        int second = minutes *60;
+        int hoursecond = hour *60*60;
+
+        System.out.println("hour to second:"+hoursecond);
+        System.out.println("minutes to second:"+second);
+    }
+}
