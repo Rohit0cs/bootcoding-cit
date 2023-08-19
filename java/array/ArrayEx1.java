73dde5d71a952bb196df22ae0f904d9b5c88089b
Rohit0cs <sakharkar004@gmail.com>
2023-08-19 11:57:06 +0530

    java program

---
+++ java/array/ArrayEx1.java
@@ -0,0 +1,13 @@
+package array;
+
+public class ArrayEx1 {
+    public static void main(String[] args) {
+        int [] temp;
+        int [] arr = {5,9,15,2,8};
+        int n =arr.length;
+
+        for(int i =0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
